591178e969d008624c3c0aa374eaded2e901634f
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2017-10-22 13:55:17 -0400

    add router to the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import config from "./config";
+import apiRouter from "./api";
 
 const server = express();
 // "dir", controller
@@ -7,9 +8,13 @@ server.get('/', (req, res) => {
   res.send('Hello Express');
 });
 
-server.get('/about.html', (req, res) => {
-  res.send('the about page');
-})
+server.use('/api', apiRouter);
+server.use(express.static('public')); //middleware
+// server.get('/about.html', (req, res) => {
+//   fs.readFile('./about.html', (err, data) => {
+//     res.send(data.toString());
+//   })
+// })
 
 server.listen(config.port, () => {
   console.log('express listening on', config.port);
