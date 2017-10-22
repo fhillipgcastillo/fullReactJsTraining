310f06ec9e8675e65b11ef1df209d6cfae0574fa
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2017-10-22 13:35:15 -0400

    playing with http server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 import config from'./config';
+
+// import https from 'https';
+// https.get('https://www.lynda.com', res => {
+//   console.log("response status: ", res.statusCode);
+//   res.on('data', chunk => {
+//     console.log(chunk.toString());
+//   });
+// });
+
+import http from "http";
+
+const server = http.createServer();
+
+server.listen(config.port);
+server.on('request', (req, res)=>{
+  res.write('hello HTTP\n');
+  setTimeout(()=>{
+    res.write('I can stream\n');
+    res.end();
+  });
+})
