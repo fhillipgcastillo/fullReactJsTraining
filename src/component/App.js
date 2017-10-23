27fadea1a67daa9ef2b243debfa336db824b6f4d
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2017-10-23 14:01:57 -0400

    add axios to call the api and get data from there

--- src/component/App.js
+++ src/component/App.js
@@ -1,23 +1,38 @@
 import  React, { Component } from "react";
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Header from "./Header";
 import ContestPreview from './ContestPreview';
+// import data from '../testData';
 
 
 class App extends Component {
   state = {
-    pageHeader: 'Naming Contest'
+    pageHeader: 'Naming Contest',
+    contests: []
   };
   // lifecicle methos
-  componentDidMount(){}/*to get data, timers, listeners*/
+  componentDidMount(){
+    // this.setState({
+    //   contests: data.contests
+    // });
+    axios.get('./api/contests')
+      .then(res => {
+        this.setState({
+          contests: res.data.contests
+        })
+      })
+      .catch(console.error);
+
+  }/*to get data, timers, listeners*/
   componentWillUnmount(){}
   render(){
     return (
       <div className="App ">
         <Header message={this.state.pageHeader}/>
         <div className="contestsPreview">
-          {this.props.contests.map( contest =>
-            <ContestPreview {...contest} /> 
+          {this.state.contests.map( contest =>
+            <ContestPreview key={contest.id} {...contest} />
           )}
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React, { PropTypes } from 'react';
 import ReactDom from 'react-dom';
 import App from "./component/App";
-import data from './testData'
 
 ReactDom.render(
-  <App headerMessage="Hello Read with JSX!" contests={data.contests} />,
+  <App />,
   document.getElementById('root')
 )
